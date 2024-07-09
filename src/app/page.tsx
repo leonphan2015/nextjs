f9fb500b55c5565542fd2a63469059ca3e366aed
Henry P <hungphamlk@gmail.com>
2024-07-09 11:04:39 +0700

    Changed Title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           priority
         />
       </div>
-
+      <div className="mb-32 "><h1 className="text-center">Welcome To Heading</h1></div>
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
